345439d48ad0faa3b3c9a9394a4ad630e59f4805
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-09-01 17:59:52 -0600

    creates classes and tests for project setup

--- battleship.rb
+++ battleship.rb
@@ -3,5 +3,5 @@ Would you like to (p)lay, read the (i)nstructions, or (q)uit? \n \n"
 
 user_input = $stdin.gets.chomp
 
-make class and run method
-game.start -> give me your first ship
+# make class and run method
+# game.start -> give me your first ship
