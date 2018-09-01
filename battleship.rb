0c2fe6c5934b3552aa07439a3710f4a3124e724e
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-09-01 16:37:16 -0600

    adds setup files for testing

---
+++ battleship.rb
@@ -0,0 +1,7 @@
+puts "Welcome to BATTLESHIP \n
+Would you like to (p)lay, read the (i)nstructions, or (q)uit? \n \n"
+
+user_input = $stdin.gets.chomp
+
+make class and run method
+game.start -> give me your first ship
