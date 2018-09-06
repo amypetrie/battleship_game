3509007b556e0751e303a16a2839721506e8513b
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-09-05 18:26:44 -0600

    deletes unused class and test files

--- test/player_test.rb
+++ test/player_test.rb
@@ -39,7 +39,7 @@ class PlayerTest < Minitest::Test
   def test_make_specific_guess
     player = Player.new("human")
     player2 = Player.new("computer")
-    player.make_specific_guess(["a", 4], player2)
+    player.make_specific_guess("a4", player2)
 
     assert_equal true, player.guess_board.layout.flatten.include?("M")
   end
